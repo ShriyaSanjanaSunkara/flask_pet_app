9553ca3e0b1eba8e5be348170335565029ad8eb3
ShriyaSanjanaSunkara <shriyasanjanasunkara@gmail.com>
2024-09-21 22:47:24 -0400

    Files modified to solve error

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
-from flask import Flask, render_template, redirect, url_for 
-from models import db, Pet 
-from forms import PetForm 
+from flask import Flask, render_template, redirect, url_for
+from models import db, Pet
+from forms import PetForm
 
-app = Flask(__name__) 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pets.db' 
-app.config['SECRET_KEY'] = 'your_secret_key' 
-db.init_app(app) 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pets.db'
+app.config['SECRET_KEY'] = 'your_secret_key'
+db.init_app(app)
 
-@app.route('/', methods=['GET', 'POST']) 
-def index(): 
-	form = PetForm() 
-	if form.validate_on_submit(): 
-		pet = Pet(name=form.name.data, age=form.age.data, type=form.type.data) 
-		db.session.add(pet) 
-		db.session.commit() 
-		return redirect(url_for('index')) 
-	pets = Pet.query.all() 
-	return render_template('view_pets.html', form=form, pets=pets) 
- 
-if __name__ == '__main__': 
-	app.run(debug=True)
+# Create the database and tables if they don't exist
+with app.app_context():
+    db.create_all()
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = PetForm()
+    if form.validate_on_submit():
+        pet = Pet(name=form.name.data, age=form.age.data, type=form.type.data)
+        db.session.add(pet)
+        db.session.commit()
+        return redirect(url_for('index'))
+    pets = Pet.query.all()
+    return render_template('view_pets.html', form=form, pets=pets)
+
+if __name__ == '__main__':
+    app.run(debug=True)
